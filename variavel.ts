7cf9b333d4ba64af364c0017a4f03e8d7d1f77dc
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 22:50:52 -0300

    example var and let

---
+++ variavel.ts
@@ -0,0 +1,13 @@
+//variaveis
+
+var x: number = 900; // var visibilidade global
+let y: string = 'marcello'; //let limitado ao bloco a visibilidade (if, switch, while ou do/while)
+var z: boolean = true;
+let r: enum;
+let c: any;
+void;
+
+var codigo = 150;
+var mensagemCupom = "Agradeçemos a preferência! Volte sempre ....";
+
+const PI: number = 3.14159265;
