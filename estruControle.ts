8f43ee74cab589f11049e95953470f657377001c
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 23:05:58 -0300

    example if, else, else if, ternario

---
+++ estruControle.ts
@@ -0,0 +1,93 @@
+var quantidadeEstoque = 1;
+
+function EmitirPedidoCompra(){}
+
+if (quantidadeEstoque < 10)
+    EmitirPedidoCompra();
+
+if (quantidadeEstoque < 10) {
+    EmitirPedidoCompra();
+}
+
+var valorCompra = 10;
+var desconto: number;
+
+if (valorCompra > 200)
+    desconto = 10;
+else
+    desconto = 5;
+
+var documentoCliente;
+var documentoFiscal;
+var dfCFe;
+var dcCPF;
+function GravarCPF(){}
+
+if (documentoFiscal == dfCFe)
+    if (documentoCliente == dcCPF)
+        GravarCPF();
+
+if(usuarioLogado){
+    if(usuarioBloqueado)
+        MensagemAviso("Bloqueado acesso a esta operação");
+}
+else
+{
+    MensagemAviso("Você precisa efetuar o login");
+}
+
+var opcaoMenu;
+function CadastrarCliente(){}
+function CadastrarFornecedor(){}
+function CadastrarProduto(){}
+function ImprimirRelatorio(){}
+
+if (opcaoMenu == 1) 
+    CadastrarCliente();
+else if (opcaoMenu == 2)
+    CadastrarFornecedor();
+else if(opcaoMenu == 3)
+    CadastrarProduto();
+else if(opcaoMenu == 4)
+    ImprimirRelatorio();
+
+
+//ternario
+
+variavel = logica ? verdadeiro:falso
+
+var numero1 = 120;
+var numero2 = 340;
+var maiorNumero = numero1 > numero2 ? numero1 : numero2;
+console.log(maiorNumero);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
