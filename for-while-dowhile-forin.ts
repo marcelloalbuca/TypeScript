83b965a8504dca7871bc88fb6694e4e04a63607a
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 23:35:38 -0300

    example for, while, do while, for in

---
+++ for-while-dowhile-forin.ts
@@ -0,0 +1,37 @@
+for (contador = 1; contador <= 100; contador++){
+    console.log(contador * 8);
+}
+
+contador = 1;
+for (contador <= 100; contador++;){
+    console.log(contador * 8);
+}
+
+contador = 1;
+for(contador <= 100;) {
+    console.log(contador * 8);
+    contador++;
+}
+
+contador = 1;
+while (contador <= 100) {
+    console.log(contador * 8);
+    contador++
+}
+
+contador = 1;
+
+do {
+    console.log(contador * 8);
+    contador++;
+}while (contador <= 100) 
+
+var matriz: number[] = [10,20,30,40,50];
+for (var elemento in matriz) {
+    console.log(matriz[elemento]);
+}
+
+
+
+
+
