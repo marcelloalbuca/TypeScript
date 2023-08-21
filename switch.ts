f42cc6302d6953054616910dc959ab303ac6b825
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 23:32:32 -0300

    example switch

---
+++ switch.ts
@@ -0,0 +1,12 @@
+switch (opcaoMenu){
+    case 1: CadastrarCliente();
+            break;
+    case 2: CadastrarFornecedor();
+            break;
+    case 3: CadastrarProduto();
+            break;
+    case 4: EmitirPedidoCompra();        
+}
+
+
+
