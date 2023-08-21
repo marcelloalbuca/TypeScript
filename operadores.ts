c856356964b13298824d458f14875a915b29a1a9
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 23:03:09 -0300

    operadores aritmeticos

---
+++ operadores.ts
@@ -0,0 +1,61 @@
+// OPERADORES ARITMÉTICOS
+// + - * / %
+
+function AnoBissexto(ano: number): boolean{
+
+    if ((ano % 4) == 0 )
+        return true;
+        else
+            return false;
+    
+}
+
+//OPERADOR RELACIONAIS
+==
+=== // IGUALDADE EXATA
+!=
+>
+<
+>=
+<=
+
+//OPERADORES LÓGICOS
+
+&&
+||
+!
+
+//OPERADORES DE ATRIBUIÇÃO
+=
+++
+--
++=
+-=
+*=
+/=
+%=
+
+var numero1: number = 1;
+numero1++;
+numero1++;
+numero1--;
+
+var numero: number = 1;
+numero = numero + 1;
+numero = numero + 1;
+numero = numero - 1;
+
+var numero: number = 1;
+numero += 1;
+numero += 1;
+numero -= 1;
+
+
+
+
+
+
+
+
+
+
