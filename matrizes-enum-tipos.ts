1b0bd4ced04bf3c9f7d92bd0aadbf3e5c4a8a681
marcelloalbuca <marcello.albuca@gmail.com>
2023-08-20 22:59:38 -0300

    example array, enum, union

---
+++ matrizes-enum-tipos.ts
@@ -0,0 +1,45 @@
+//array - matriz
+var despesaMensal: number[];
+
+                        //tipo de dados        
+var despesaMensal: Array<number>;
+
+
+var codigoOperacao: number[] = [101, 200, 320, 448];
+var tipoMovimentacao: Array<string> = ["Entrada", "Saída", "Estorno"];
+
+var codigoImposto[5] = 100;
+
+var matrizValores: any[] =["001.002.003-04", 412, true, 280.50];
+
+//ENUM
+
+enum naturezaOperacao {Entrada, Devolucao, Venda, Perda, Estorno, SimplesRemessa};
+
+var operacao: naturezaOperacao;
+operacao = naturezaOperacao.Entrada;
+
+//UNION
+
+//apenas numeros
+var indiceCalculo: number | number[];
+indiceCalculo = 10;
+indiceCalculo = [5,8,12];
+
+type coordenadas: number;
+type textoMensagem: string;
+type codigoRetorno: Array<number>;
+
+var posicaoX, posicaoY: coordenadas;
+var mensagem: textoMensagem = "Aguarde o processamento...";
+var retornos: codigoRetorno = [10, 21, 34, 58];
+
+
+
+
+
+
+
+
+
+
